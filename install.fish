823a1145deae7c7c19b8171e3beba51f393b76b7
Zhe Weng <wengzhe@xiaomi.com>
2025-10-16 18:43:23 +0800

    Add support for Android (#1)
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- install.fish
+++ install.fish
@@ -10,11 +10,18 @@ function xlunch --description 'bash envsetup.sh + lunch + export env'
         source build/envsetup.sh
         lunch $argv
         cat > .xenv.sh <<EOF
+# Vela
 export VELA_BUILD_TARGET_BOARD=\"\$VELA_BUILD_TARGET_BOARD\"
 export VELA_BUILD_TARGET_VENDOR=\"\$VELA_BUILD_TARGET_VENDOR\"
 export VELA_BUILD_TARGET_CONFIG=\"\$VELA_BUILD_TARGET_CONFIG\"
 export CURRENT_LUNCH_BINARY_DIR=\"\$CURRENT_LUNCH_BINARY_DIR\"
 export TARGET_BUILD_APPS=\"\$TARGET_BUILD_APPS\"
+# Android
+export TARGET_BUILD_TYPE=\"\$TARGET_BUILD_TYPE\"
+export TARGET_BUILD_VARIANT=\"\$TARGET_BUILD_VARIANT\"
+export TARGET_PRODUCT=\"\$TARGET_PRODUCT\"
+export TARGET_RELEASE=\"\$TARGET_RELEASE\"
+set_stuff_for_environment
 EOF
     "
 end
